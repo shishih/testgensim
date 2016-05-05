27af69a25801a324edea1f0ec097b4a94cc8faf4
shishih <agnesyao1110@gmail.com>
2016-05-05 10:23:16 +0800

    finish first train

--- tegen.py
+++ tegen.py
@@ -6,6 +6,7 @@ from gensim.models.word2vec import LineSentence
 from gensim.models.word2vec import train_batch_sg
 import numpy
 from collections import Counter
+import re
 # from gensim.models.word2vec_inner import train_batch_sg
 
 def te():
@@ -39,10 +40,11 @@ def teword():
 def setwordwindow(windowsize):
     tmpstrp=' 1'*windowsize
     tmpstrn=' -1'*windowsize
+    pattern=re.compile(r' ')
     
     files=['corpus/positive','corpus/negative']
     with open('corpus/initindex'+str(windowsize),'w+') as fwrite:
-        fwrite.write('18164 '+str(windowsize)+'\n')      
+        fwrite.write('15289 '+str(windowsize)+'\n')      
         with open(files[0]) as fp:
             datap=fp.readlines()
             print datap
@@ -56,7 +58,8 @@ def setwordwindow(windowsize):
 
         for i in counter:
             if (counter[i] == 1):
-                fwrite.write(i[:-1])
+                i=re.sub(r'\s+','',i)
+                fwrite.write(i)
                 if i in datap:
                     fwrite.write(tmpstrp)
                 else:
@@ -66,37 +69,41 @@ def setwordwindow(windowsize):
 
 def intersect(windowsize): 
     # merged OK!   
-    windowsize=40
-    # model=Word2Vec(size=windowsize,min_count=2,sg=1)
+    # windowsize=40
+    model=Word2Vec(size=windowsize,min_count=2,sg=1)
 
-    # sentences=LineSentence('corpus/precorpus')
-    # model.build_vocab(sentences)
-    # model.train(sentences)
-    # print 'finish pre-train'
-    # model.save('corpus/pretrain'+str(windowsize)+'.model')
-    # model.save_word2vec_format('corpus/pretrain'+str(windowsize))
+    sentences=LineSentence('corpus/precorpus')
+    model.build_vocab(sentences)
+    model.train(sentences)
+    print 'finish pre-train'
+    model.save('corpus/pretrain'+str(windowsize)+'.model')
+    model.save_word2vec_format('corpus/pretrain'+str(windowsize))
     
     # intersect does not delete the bibary tree, but load does
-    model=Word2Vec.load('corpus/pretrain'+str(windowsize)+'.model')
+    # model=Word2Vec.load('corpus/pretrain'+str(windowsize)+'.model')
     setwordwindow(windowsize)
     Word2Vec.intersect_word2vec_format(model,'corpus/initindex'+str(windowsize),binary=False)
     print 'finish intersect'
     model.save('corpus/merged'+str(windowsize)+'.model')
     model.save_word2vec_format('corpus/merged'+str(windowsize), binary=False)
-    # model=Word2Vec.load_word2vec_format('temerged',binary=False) 
+
 
     # model.build_vocab(sentences)
 
-    sensum=0
-    for i in sentences:
-        sensum+=1
-    model.corpus_count=sensum
+    # sensum=0
+    # for i in sentences:
+    #     sensum+=1
+    # model.corpus_count=sensum
 
     # Word2Vec.reset_weights(model)
+    # model=Word2Vec.load('corpus/merged40.model')
+    print "finish load"
     sentences=LineSentence('corpus/fieldcorpus')
+    print "finish sentence building"
     
     model.iter=1
     model.train(sentences)
+    print "finish training"
 
     # in class Word2Vec
     # self.build_vocab(sentences, trim_rule=trim_rule)
@@ -105,16 +112,20 @@ def intersect(windowsize):
 
     # train_batch_sg(model, sentences, alpha=0.1,work=None)
     # simply use train and set iter=1?
-    model.save('mergedtrained'+str(windowsize)+'iter'+str(model.iter)+'.model')
-    model.save_word2vec_format('mergedtrained'+str(windowsize)+'iter'+str(model.iter), binary=False)
+    model.save('corpus/mergedtrained'+str(windowsize)+'iter'+str(model.iter)+'.model')
+    model.save_word2vec_format('corpus/mergedtrained'+str(windowsize)+'iter'+str(model.iter), binary=False)
 
+def dis(model):
+    print model.similarity("今天","坏")
 
 def main():
     # te()
     # teword()
-    intersect(40)
+    # intersect(40)
     # setwordwindow(40)
-    # Word2Vec.load_word2vec_format('initindex.txt',binary=False)
+    # Word2Vec.load_word2vec_format('corpus/initindex40',binary=False)
+    model=Word2Vec.load('corpus/mergedtrained40iter1.model')
+    dis(model)
     
 
 if __name__ == '__main__':
